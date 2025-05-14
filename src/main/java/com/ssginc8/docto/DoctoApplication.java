e9f8acb194d199517cbbc1c745ee9f5b5fe01ba9
Soomin <o3o_121@naver.com>
2025-05-14 11:40:31 +0900

    🔧 Chore: 프로젝트 초기 세팅 및 기본 폴더 구조 구성

---
+++ src/main/java/com/ssginc8/docto/DoctoApplication.java
@@ -0,0 +1,13 @@
+package com.ssginc8.docto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssginc8/docto/DoctoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssginc8.docto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
