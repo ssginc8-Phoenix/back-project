1d2b69f5f0cda3e44ec5c0e9b5e66cbb95b29ed9
Soomin <o3o_121@naver.com>
2025-05-16 09:08:04 +0900

    ♻️ Refactor: 엔티티 기본 생성자 접근 제어자를 protected로 변경

--- src/main/java/com/ssginc8/docto/doctor/entity/Doctor.java
+++ src/main/java/com/ssginc8/docto/doctor/entity/Doctor.java
@@ -15,18 +15,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 @Table
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Doctor extends BaseTimeEntity {
 
