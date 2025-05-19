7bd9514242271fc48ff081d0cd1bf00dc2a100c3
jiiinchoi <hanuel001029@naver.com>
2025-05-19 13:25:53 +0900

    ♻️ Refactor: ReviewDTO 코드 수정

---
+++ src/main/java/com/ssginc8/docto/review/dto/ReviewCreateRequest.java
@@ -0,0 +1,31 @@
+package com.ssginc8.docto.review.dto;
+
+import java.util.List;
+
+import com.ssginc8.docto.review.entity.KeywordType;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+
+@Data
+public class ReviewCreateRequest {
+
+
+	private Long appointmentId;
+	private Long hospitalId;
+	private Long doctorId;
+	private Long userId;
+
+
+	@NotBlank(message = "content는 빈 값일 수 없습니다.")
+	@Size(max = 1000, message = "content는 최대 1000자까지 입력 가능합니다.")
+	private String content;
+
+	@NotNull(message = "키워드를 최소 3개 선택해주세요.")
+	@Size(min = 3, max = 8, message = "키워드는 3~8개 사이로 선택 가능합니다.")
+	private List<KeywordType> keyword;
+
+}
---
+++ src/main/java/com/ssginc8/docto/review/dto/ReviewUpdateRequest.java
@@ -0,0 +1,24 @@
+package com.ssginc8.docto.review.dto;
+
+import java.util.List;
+
+import com.ssginc8.docto.review.entity.KeywordType;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+
+//수정 가능한 것은 내용이랑 키워드, 수정 가능한 필드만 Body로 받고, ID는 URL PathVariable
+@Data
+public class ReviewUpdateRequest{
+
+	@NotBlank(message = "contents는 빈 값일 수 없습니다.")
+	@Size(max = 1000, message = "contents는 최대 1000자까지 입력 가능합니다.")
+	private String contents;
+
+	@Size(min = 3, max = 8, message = "키워드는 3~8개 사이로 선택 가능합니다.")
+	private List<KeywordType> keywords;
+
+
+}
