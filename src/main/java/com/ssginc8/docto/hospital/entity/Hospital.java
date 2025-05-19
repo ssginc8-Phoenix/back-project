4116eb3cc417a44acc69dd80be1d44b73a04f9e9
jaehoney01 <wogjse0321@naver.com>
2025-05-19 17:18:45 +0900

    ✨ Feat : 정적 팩토리 메서드 추가
    # Conflicts:
    #	src/main/java/com/ssginc8/docto/hospital/entity/HospitalSchedule.java

--- src/main/java/com/ssginc8/docto/hospital/entity/Hospital.java
+++ src/main/java/com/ssginc8/docto/hospital/entity/Hospital.java
@@ -1,10 +1,13 @@
 package com.ssginc8.docto.hospital.entity;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.ssginc8.docto.global.base.BaseTimeEntity;
 import com.ssginc8.docto.user.entity.User;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -12,16 +15,26 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
+
+
 @Table(name = "tbl_hospital")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Setter
+
 public class Hospital extends BaseTimeEntity {
 
 	@Id
@@ -57,4 +70,31 @@ public class Hospital extends BaseTimeEntity {
 
 	@Column(nullable = false, length = 200)
 	private String businessRegistrationNumber;
+
+	public static Hospital create(User user,
+		String name,
+		String address,
+		BigDecimal latitude,
+		BigDecimal longitude,
+		String phone,
+		String introduction,
+		String notice,
+		Long waiting,
+		String businessRegistrationNumber) {
+		Hospital hospital = new Hospital();
+		hospital.setUser(user);
+		hospital.setName(name);
+		hospital.setAddress(address);
+		hospital.setLatitude(latitude);
+		hospital.setLongitude(longitude);
+		hospital.setPhone(phone);
+		hospital.setIntroduction(introduction);
+		hospital.setNotice(notice);
+		hospital.setWaiting(waiting);
+		hospital.setBusinessRegistrationNumber(businessRegistrationNumber);
+		return hospital;
+	}
+
+
+
 }
--- src/main/java/com/ssginc8/docto/hospital/entity/HospitalSchedule.java
+++ src/main/java/com/ssginc8/docto/hospital/entity/HospitalSchedule.java
@@ -1,12 +1,13 @@
 package com.ssginc8.docto.hospital.entity;
 
 import java.time.DayOfWeek;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,10 +15,15 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+
+
+
 @Table(name = "tbl_hospital_schedule")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -27,23 +33,40 @@ public class HospitalSchedule {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long hospitalScheduleId;
 
-	@ManyToOne
-	@JoinColumn(name = "hospitalId", nullable = false)
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "hospital_id", nullable = false)
 	private Hospital hospital;
 
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private DayOfWeek dayOfWeek;
 
-	@Column(nullable = false)
-	private LocalDateTime openTime;
+	@Column(nullable = false, name = "open_time")
+	private LocalTime openTime;
 
-	@Column(nullable = false)
-	private LocalDateTime closeTime;
+	@Column(nullable = false, name = "close_time")
+	private LocalTime closeTime;
 
-	@Column(nullable = false)
-	private LocalDateTime lunchStart;
+	@Column(nullable = false, name = "lunch_start")
+	private LocalTime lunchStart;
 
-	@Column(nullable = false)
-	private LocalDateTime lunchEnd;
+	@Column(nullable = false, name = "lunch_end")
+	private LocalTime lunchEnd;
+
+	public static HospitalSchedule create(Hospital hospital,
+		DayOfWeek dayOfWeek,
+		LocalTime openTime,
+		LocalTime closeTime,
+		LocalTime lunchStart,
+		LocalTime lunchEnd) {
+		HospitalSchedule schedule = new HospitalSchedule();
+		schedule.hospital = hospital;
+		schedule.dayOfWeek = dayOfWeek;
+		schedule.openTime = openTime;
+		schedule.closeTime = closeTime;
+		schedule.lunchStart = lunchStart;
+		schedule.lunchEnd = lunchEnd;
+		return schedule;
+	}
 }
