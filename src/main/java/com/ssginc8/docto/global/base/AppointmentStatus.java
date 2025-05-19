08e5fef5c043255f0831552bfedaacd2ea03f9c7
Soomin <o3o_121@naver.com>
2025-05-19 17:16:17 +0900

    ♻️ Refactor: ERD 수정

--- src/main/java/com/ssginc8/docto/global/base/AppointmentStatus.java
+++
@@ -1,7 +0,0 @@
-package com.ssginc8.docto.global.base;
-
-public enum AppointmentStatus {
-
-	// 예약완료, 대기중, 진료완료, 취소
-	Requested, Confirmed, Waiting, Completed, Cancelled;
-}
--- src/main/java/com/ssginc8/docto/global/base/DayOfWeek.java
+++
@@ -1,6 +0,0 @@
-package com.ssginc8.docto.global.base;
-
-public enum DayOfWeek {
-
-	MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
-}
