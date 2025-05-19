6149d719d56b4b0a58795752547493ebdefbce91
JM <pizzashim@gmail.com>
2025-05-19 17:13:09 +0900

    💬 Comment: Status Enum 주석 추가

--- src/main/java/com/ssginc8/docto/guardian/entity/Status.java
+++ src/main/java/com/ssginc8/docto/guardian/entity/Status.java
@@ -2,5 +2,11 @@ package com.ssginc8.docto.guardian.entity;
 
 public enum Status {
 
+	/**
+	 * 보호자 권한 상태 Enum
+	 * - Pending: 요청 대기 중
+	 * - Accepted: 수락됨
+	 * - Rejected: 거절됨
+	 */
 	Pending, Accepted, Rejected;
 }
