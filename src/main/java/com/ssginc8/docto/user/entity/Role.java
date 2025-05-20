4367b32fb71a47de4cb4bd16b3244cfc939cf158
dongdong8343 <fkdlem8343@naver.com>
2025-05-20 14:55:32 +0900

    🔧 Chore: Role 속성(key) 추가

--- src/main/java/com/ssginc8/docto/user/entity/Role.java
+++ src/main/java/com/ssginc8/docto/user/entity/Role.java
@@ -1,17 +1,18 @@
 package com.ssginc8.docto.user.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
 
-	PATIENT("환자"),
-	GUARDIAN("보호자"),
-	HOSPITAL_ADMIN("병원 관리자"),
-	DOCTOR("의사"),
-	SYSTEM_ADMIN("시스템 관리자")
-	;
+	PATIENT("ROLE_PATIENT", "환자"),
+	GUARDIAN("ROLE_GUARDIAN", "보호자"),
+	HOSPITAL_ADMIN("ROLE_HOSPITAL_ADMIN", "병원 관리자"),
+	DOCTOR("ROLE_DOCTOR", "의사"),
+	SYSTEM_ADMIN("ROLE_SYSTEM_ADMIN", "시스템 관리자");
 
-	private final String label;
-
-	Role(String label) {
-		this.label = label;
-	}
+	private final String key;
+	private final String title;
 }
