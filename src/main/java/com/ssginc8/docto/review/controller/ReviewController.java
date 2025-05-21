e977ce60d9b050c8bc4c4c2edd33b42d26921bc2
jiiinchoi <hanuel001029@naver.com>
2025-05-21 17:23:58 +0900

    ♻️ Refactor: ReviewService, ReviewController 수정

--- src/main/java/com/ssginc8/docto/review/controller/ReviewController.java
+++ src/main/java/com/ssginc8/docto/review/controller/ReviewController.java
@@ -1,17 +1,15 @@
 package com.ssginc8.docto.review.controller;
 
-import java.util.List;
-import java.util.function.Function;
+import org.springframework.data.domain.Pageable;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.ssginc8.docto.review.dto.ReviewAllListResponse;
 import com.ssginc8.docto.review.dto.ReviewCreateRequest;
-import com.ssginc8.docto.review.dto.ReviewResponse;
+import com.ssginc8.docto.review.dto.ReviewMyListResponse;
 import com.ssginc8.docto.review.dto.ReviewUpdateRequest;
-import com.ssginc8.docto.review.entity.Review;
-import com.ssginc8.docto.review.entity.ReviewKeyword;
-import com.ssginc8.docto.review.repository.ReviewKeywordRepo;
 import com.ssginc8.docto.review.service.ReviewServiceImpl;
 
 import lombok.RequiredArgsConstructor;
@@ -22,23 +20,19 @@ import lombok.RequiredArgsConstructor;
 public class ReviewController {
 
 	private final ReviewServiceImpl reviewService;
-	private final ReviewKeywordRepo reviewKeywordRepo;
 
-	// 리뷰 작성
+	// 리뷰 생성
 	@PostMapping("/reviews")
 	public ResponseEntity<Long> create(@RequestBody ReviewCreateRequest dto) {
 		Long id = reviewService.createReview(dto, dto.getUserId());
+		//리뷰 아이디 반환
 		return ResponseEntity.ok(id);
 	}
 
-
 	// 리뷰 수정
 	@PatchMapping("/reviews/{reviewId}")
-	public ResponseEntity<Void> update(
-		@PathVariable Long reviewId,
-		@RequestBody ReviewUpdateRequest dto
-	) {
-		reviewService.updateReview(reviewId, dto);
+	public ResponseEntity<Void> update(@PathVariable Long reviewId, @RequestBody ReviewUpdateRequest dto
+	) {reviewService.updateReview(dto,reviewId);
 		return ResponseEntity.noContent().build();
 	}
 
@@ -49,36 +43,25 @@ public class ReviewController {
 		return ResponseEntity.noContent().build();
 	}
 
-	// 나의 리뷰 조회
+	//나의 예약 조회 (/api/v1/users/me/reviews?userId=7&page=0&size)
 	@GetMapping("/users/me/reviews")
-	public ResponseEntity<List<ReviewResponse>> getMyReviews(@RequestParam Long userId) {
-		List<Review> reviews = reviewService.getMyReviews(userId);
-
-		Function<Review, List<String>> kwExtractor = r ->
-			reviewKeywordRepo.findByReview(r)
-				.stream()
-				.map(ReviewKeyword::getKeywords)
-				.map(Enum::name)
-				.toList();
-
-		List<ReviewResponse> dtoList = ReviewResponse.fromEntities(reviews, kwExtractor);
-		return ResponseEntity.ok(dtoList);
+	public ResponseEntity<Page<ReviewMyListResponse>> getMyReviews(Pageable pageable
+	) {Page<ReviewMyListResponse> page = reviewService.getMyReviews(pageable);
+		return ResponseEntity.ok(page);
 	}
 
-	// 병원관리자용 전체 리뷰 조회
-	@GetMapping("/reviews")
-	public ResponseEntity<List<ReviewResponse>> getAllReviews() {
-		List<Review> reviews = reviewService.getAllReviews();
-
-		// 리뷰 → 키워드 문자열 리스트 추출 함수
-		Function<Review, List<String>> kwExtractor = r ->
-			reviewKeywordRepo.findByReview(r)
-				.stream()
-				.map(ReviewKeyword::getKeywords)
-				.map(Enum::name)
-				.toList();
-
-		List<ReviewResponse> dtoList = ReviewResponse.fromEntities(reviews, kwExtractor);
-		return ResponseEntity.ok(dtoList);
+	//병원 리스트 에서 전체 리뷰 조회
+	@GetMapping("/hospitals/{hospitalId}/reviews")
+	public ResponseEntity<Page<ReviewMyListResponse>> getAllReviews(@PathVariable Long hospitalId, Pageable pageable
+	) {Page<ReviewAllListResponse> page = reviewService.getAllReviews(hospitalId,pageable);
+		return ResponseEntity.ok(page);
 	}
+
 }
+
+
+
+
+
+
+
--- src/main/java/com/ssginc8/docto/review/service/ReviewService.java
+++ src/main/java/com/ssginc8/docto/review/service/ReviewService.java
@@ -1,15 +1,13 @@
 package com.ssginc8.docto.review.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
-import org.springframework.transaction.annotation.Transactional;
-
+import com.ssginc8.docto.review.dto.ReviewAllListResponse;
 import com.ssginc8.docto.review.dto.ReviewCreateRequest;
+import com.ssginc8.docto.review.dto.ReviewMyListResponse;
+import com.ssginc8.docto.review.dto.ReviewResponse;
 import com.ssginc8.docto.review.dto.ReviewUpdateRequest;
-import com.ssginc8.docto.review.entity.Review;
-
-//쓰기(생성·수정·삭제) 기능을 처리
 
 public interface ReviewService {
 
@@ -17,15 +15,24 @@ public interface ReviewService {
 	Long createReview(ReviewCreateRequest request, Long userId);
 
 	//리뷰 수정
-	void updateReview(Long reviewId, ReviewUpdateRequest request);
+	ReviewResponse updateReview( ReviewUpdateRequest request, Long ReviewId);
 
 	//리뷰 삭제
-	@Transactional
 	void deleteReview(Long reviewId);
 
 	// 내 리뷰 목록 조회
-	List<Review> getMyReviews(Long userId);
+	Page<ReviewMyListResponse> getMyReviews(Pageable pageable);
+
 
 	// (관리자용) 전체 리뷰 목록 조회
-	List<Review> getAllReviews();
+	Page<ReviewAllListResponse> getAllReviews(Long HospitalId, Pageable pageable);
+
+
 }
+
+
+
+
+
+
+
