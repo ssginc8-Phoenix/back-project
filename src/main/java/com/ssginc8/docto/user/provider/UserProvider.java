40bb4efdf69e78269d78da0f351edd7ab7e33f20
dongdong8343 <fkdlem8343@naver.com>
2025-05-21 09:41:43 +0900

    🔧 Chore: UserRepository -> UserRepo

--- src/main/java/com/ssginc8/docto/user/provider/UserProvider.java
+++ src/main/java/com/ssginc8/docto/user/provider/UserProvider.java
@@ -6,25 +6,25 @@ import org.springframework.stereotype.Service;
 
 import com.ssginc8.docto.global.error.exception.userException.UserNotFoundException;
 import com.ssginc8.docto.user.entity.User;
-import com.ssginc8.docto.user.repository.UserRepository;
+import com.ssginc8.docto.user.repository.UserRepo;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class UserProvider {
-	private final UserRepository userRepository;
+	private final UserRepo userRepo;
 
 	// 이메일 중복 검사
 	public Optional<User> checkEmail(String email) {
-		return userRepository.findByEmail(email);
+		return userRepo.findByEmail(email);
 	}
 
 	public User loadUserByEmail(String email) {
-		return userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
+		return userRepo.findByEmail(email).orElseThrow(UserNotFoundException::new);
 	}
 
 	public User createUser(User user) {
-		return userRepository.save(user);
+		return userRepo.save(user);
 	}
 }
--- src/main/java/com/ssginc8/docto/user/repository/UserRepo.java
+++ src/main/java/com/ssginc8/docto/user/repository/UserRepo.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssginc8.docto.user.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepo extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
 }
