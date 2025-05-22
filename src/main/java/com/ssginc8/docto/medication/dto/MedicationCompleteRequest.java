814702b3e3441be531b749a7c408e9cac548b1e0
JM <pizzashim@gmail.com>
2025-05-22 16:44:18 +0900

    ♻️ Refactor: #4 Lombok 애노테이션(@Builder 등) 적용으로 생성자 및 빌더 자동화

--- src/main/java/com/ssginc8/docto/medication/dto/MedicationCompleteRequest.java
+++ src/main/java/com/ssginc8/docto/medication/dto/MedicationCompleteRequest.java
@@ -2,7 +2,10 @@ package com.ssginc8.docto.medication.dto;
 
 import com.ssginc8.docto.medication.entity.MedicationStatus;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 복약 완료 처리 요청 DTO
@@ -10,6 +13,9 @@ import lombok.Getter;
  * - 복용 상태를 클라이언트에서 전달 (TAKEN, MISSED)
  */
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MedicationCompleteRequest {
 
 	private MedicationStatus status; // 복용 상태
--- src/main/java/com/ssginc8/docto/medication/dto/MedicationScheduleRequest.java
+++ src/main/java/com/ssginc8/docto/medication/dto/MedicationScheduleRequest.java
@@ -4,7 +4,9 @@ import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 약 복용 스케줄 등록 요청 DTO
@@ -17,10 +19,20 @@ import lombok.Getter;
  * 클라이언트가 약을 등록할 때 요청 본문으로 사용
  */
 @Getter
+@NoArgsConstructor
+@Builder
 public class MedicationScheduleRequest {
 
 	private Long patientGuardianId;          // 환자-보호자 관계 ID
 	private String medicationName;           // 약 이름
 	private LocalDateTime timeToTake;        // 복용 시간
 	private List<DayOfWeek> days;            // 복용 요일 리스트
+
+	public MedicationScheduleRequest(Long patientGuardianId, String medicationName,
+		LocalDateTime timeToTake, List<DayOfWeek> days) {
+		this.patientGuardianId = patientGuardianId;
+		this.medicationName = medicationName;
+		this.timeToTake = timeToTake;
+		this.days = days;
+	}
 }
