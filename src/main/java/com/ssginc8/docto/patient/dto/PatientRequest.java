8c7af49f69df87cf9dfcee7a34b556da16e9fe05
JM <pizzashim@gmail.com>
2025-05-23 16:13:21 +0900

    ♻️ Refactor: #11환자 dto의 기능 개선

--- src/main/java/com/ssginc8/docto/patient/dto/PatientRequest.java
+++ src/main/java/com/ssginc8/docto/patient/dto/PatientRequest.java
@@ -1,12 +1,15 @@
 package com.ssginc8.docto.patient.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 /**
  * 환자 등록 요청 DTO
  */
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PatientRequest {
 	private Long userId;
 	private String residentRegistrationNumber;
