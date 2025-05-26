96f193378d6cef3971de8e74ec07d3a34994d294
jaehoney01 <wogjse0321@naver.com>
2025-05-26 15:17:45 +0900

    🗑️ Remove : 중복 파일 제거

--- src/main/java/com/ssginc8/docto/hospital/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssginc8.docto.hospital.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssginc8.docto.user.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long > {
-	boolean existsByEmail(String email);
-}
