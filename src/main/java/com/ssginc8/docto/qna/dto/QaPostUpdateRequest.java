611439f77a4d974346975328eddf1055301f73e8
jiiinchoi <hanuel001029@naver.com>
2025-05-26 11:16:23 +0900

    ✨ Feat: QNA DTO 추가

---
+++ src/main/java/com/ssginc8/docto/qna/dto/QaPostUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.ssginc8.docto.qna.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class QaPostUpdateRequest {
+
+
+	private Long appointmentId;
+
+	@NotBlank(message = "content는 빈 값일 수 없습니다.")
+	@Size(max = 500, message = "content는 최대 500자까지 입력 가능합니다.")
+	private String content;
+
+}
