35f167fc53cd1944057d54e7b0300254eff3f152
jiiinchoi <hanuel001029@naver.com>
2025-05-26 11:11:40 +0900

    ✨ Feat: QNA 서비스 구현체 추가

--- src/main/java/com/ssginc8/docto/qna/service/QaPostServiceImpl.java
+++ src/main/java/com/ssginc8/docto/qna/service/QaPostServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ssginc8.docto.appointment.entity.Appointment;
+import com.ssginc8.docto.qna.dto.QaPostResponse;
 import com.ssginc8.docto.qna.entity.QaPost;
 import com.ssginc8.docto.qna.provider.QaPostProvider;
 
@@ -20,5 +21,26 @@ public class QaPostServiceImpl implements QaPostService{
 	public QaPost createQaPost(Appointment appointment, String content) {
 		QaPost post = QaPost.create(appointment, content);
 		return qaPostProvider.save(post);
+	}
+
+	@Override
+	@Transactional
+	public QaPostResponse updateQaPost(Long qnaId, String content) {
+		QaPost existing = qaPostProvider.getById(qnaId);
+		existing.setContent(content);
+		QaPost saved = qaPostProvider.save(existing);
+		return QaPostResponse.fromEntity(saved);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public QaPostResponse getQaPost(Long qnaId) {
+		return QaPostResponse.fromEntity(qaPostProvider.getById(qnaId));
+	}
+
+	@Override
+	@Transactional
+	public void deleteQaPost(Long qnaId) {
+		qaPostProvider.deleteById(qnaId);
 	}
 }
