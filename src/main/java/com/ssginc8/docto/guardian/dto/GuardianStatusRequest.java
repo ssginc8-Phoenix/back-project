dbb97a57b043c371314196f272f555abe26bf7ab
JM <pizzashim@gmail.com>
2025-05-26 11:24:16 +0900

    ♻️ Refactor: #16 GuardianStatusRequest에 @Builder 추가 및 코드 개선

--- src/main/java/com/ssginc8/docto/guardian/dto/GuardianStatusRequest.java
+++ src/main/java/com/ssginc8/docto/guardian/dto/GuardianStatusRequest.java
@@ -1,14 +1,20 @@
 package com.ssginc8.docto.guardian.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
  * 보호자 권한 상태 변경 요청 DTO
  * status 값으로 "ACCEPTED" 또는 "REJECTED" 문자열을 받음
+ * - inviteCode: 엔티티에 저장된 초대 코드 (User.uuid)
  */
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GuardianStatusRequest {
 	private String status;
+	private String inviteCode;
 }
