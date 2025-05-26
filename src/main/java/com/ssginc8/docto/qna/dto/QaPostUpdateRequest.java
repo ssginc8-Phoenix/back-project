f46de3c1f2aaca3340ba54799b033152d88f499c
jiiinchoi <hanuel001029@naver.com>
2025-05-26 16:09:32 +0900

    ♻️ Refactor: QNA DTO 코드 수정

--- src/main/java/com/ssginc8/docto/qna/dto/QaPostUpdateRequest.java
+++ src/main/java/com/ssginc8/docto/qna/dto/QaPostUpdateRequest.java
@@ -10,8 +10,6 @@ import lombok.Data;
 public class QaPostUpdateRequest {
 
 
-	private Long appointmentId;
-
 	@NotBlank(message = "content는 빈 값일 수 없습니다.")
 	@Size(max = 500, message = "content는 최대 500자까지 입력 가능합니다.")
 	private String content;
