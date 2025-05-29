74b33f33b5fd1b8eb96450fa6f7067f1f3e9818f
Soomin <o3o_121@naver.com>
2025-05-29 15:46:45 +0900

    ✅ Test: prod 설정 추가

--- src/test/java/com/ssginc8/docto/appointment/controller/AppointmentControllerTest.java
+++ src/test/java/com/ssginc8/docto/appointment/controller/AppointmentControllerTest.java
@@ -21,6 +21,7 @@ import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -33,6 +34,7 @@ import com.ssginc8.docto.appointment.dto.RescheduleRequest;
 import com.ssginc8.docto.appointment.dto.UpdateRequest;
 import com.ssginc8.docto.restdocs.RestDocsConfig;
 
+@ActiveProfiles("prod")
 @ExtendWith(RestDocumentationExtension.class)
 @SpringBootTest
 @Import(RestDocsConfig.class)
