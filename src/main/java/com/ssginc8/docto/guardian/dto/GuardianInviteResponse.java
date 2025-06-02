2e758e665ba03edac18e3406c7684c8a888e6ecf
JM <pizzashim@gmail.com>
2025-06-02 09:49:46 +0900

    ...

---
+++ src/main/java/com/ssginc8/docto/guardian/dto/GuardianInviteResponse.java
@@ -0,0 +1,14 @@
+package com.ssginc8.docto.guardian.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 보호자 초대 응답 DTO
+ */
+@Getter
+@AllArgsConstructor
+public class GuardianInviteResponse {
+
+	private String inviteCode;
+}
