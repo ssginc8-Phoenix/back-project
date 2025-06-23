ca37ea677a927867e72b952246af5a9b993ad5fb
Soomin <o3o_121@naver.com>
2025-06-23 22:15:21 +0900

    ♻️ Refactor: 사용하지 않는 변수 삭제

--- src/main/java/com/ssginc8/docto/hospital/entity/Hospital.java
+++ src/main/java/com/ssginc8/docto/hospital/entity/Hospital.java
@@ -125,6 +125,10 @@ public class Hospital extends BaseTimeEntity {
 		this.waiting = waiting;
 	}
 
+	// @Enumerated(EnumType.STRING)
+	// @Column(nullable = false)
+	// private Specialization specialization;
+
 
 
 	public void clearFileIds() {
