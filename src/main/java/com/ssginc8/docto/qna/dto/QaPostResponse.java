e5bdec37fd9b8bb561ddb8fc81d8c64ee65b978b
jiiinchoi <hanuel001029@naver.com>
2025-06-30 10:53:03 +0900

    ♻️ Refactor: QNA response dto 확장

--- src/main/java/com/ssginc8/docto/qna/dto/QaPostResponse.java
+++ src/main/java/com/ssginc8/docto/qna/dto/QaPostResponse.java
@@ -1,6 +1,8 @@
 package com.ssginc8.docto.qna.dto;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import com.ssginc8.docto.qna.entity.QaPost;
 import com.ssginc8.docto.qna.entity.QaStatus;
@@ -25,7 +27,14 @@ public class QaPostResponse {
 	private LocalDateTime createdAt;
 	private LocalDateTime updatedAt;
 
+	private String guardianName;
+	private String patientName;
+	private String symptom;
+	private String appointmentTime;
+
 	public static QaPostResponse fromEntity(QaPost post) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd(E) HH:mm", Locale.KOREAN);
+
 		QaPostResponse dto = new QaPostResponse();
 		dto.qnaPostId     = post.getQnaPostId();
 		dto.appointmentId = post.getAppointment().getAppointmentId();
@@ -33,6 +42,11 @@ public class QaPostResponse {
 		dto.status        = post.getStatus();
 		dto.createdAt     = post.getCreatedAt();
 		dto.updatedAt     = post.getUpdatedAt();
+		dto.guardianName     = post.getAppointment().getPatientGuardian().getUser().getName();
+		dto.patientName      = post.getAppointment().getPatientGuardian().getPatient().getUser().getName();
+		dto.symptom          = post.getAppointment().getSymptom();
+		dto.appointmentTime  = post.getAppointment().getAppointmentTime().format(formatter);
+
 		return dto;
 	}
 }
