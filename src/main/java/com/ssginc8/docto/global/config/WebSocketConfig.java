3a6c938df5b628a524325271146fa27d93e30021
jaehoney01 <wogjse0321@naver.com>
2025-07-06 12:09:26 +0900

    Revert "♻️ Refactor : 웹소켓 연결 개선"
    This reverts commit e66db044a9e0f0ba88b9b543a639b18c713ca9fd.

--- src/main/java/com/ssginc8/docto/global/config/WebSocketConfig.java
+++ src/main/java/com/ssginc8/docto/global/config/WebSocketConfig.java
@@ -6,7 +6,6 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,8 +29,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	 */
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry){
-		registry.addEndpoint("/ws-chat")
-			.addInterceptors(new HttpSessionHandshakeInterceptor())
-			.setAllowedOriginPatterns("http://localhost:5173").withSockJS();
+		registry.addEndpoint("/ws-chat").setAllowedOrigins("*").withSockJS();
 	}
 }
